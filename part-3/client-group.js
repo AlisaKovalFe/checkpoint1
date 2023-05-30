1ebc48672c091e2b5be8e6aa38a009d954f53244
Alisa <alisakovalhome@mail.ru>
2023-05-30 17:28:29 +0300

    Add new in client-group

--- part-3/client-group.js
+++ part-3/client-group.js
@@ -1,6 +1,7 @@
 const PersonGroup = require('./person-group')
 module.exports = class ClientGroup extends PersonGroup {
-  get clients() {
-    return this.persons       //почему здесь не нужен констуктор и вот надо просто геттером
+  constructor(persons) {
+    super(persons)
+    this.clients = persons
   }
 };
